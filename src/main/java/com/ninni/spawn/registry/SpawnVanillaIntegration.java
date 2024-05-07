db33ce20d3f472b9ee917c2ce3a28628607dd348
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2024-05-07 19:42:05 +0200

    divided the packs and squid retexture

--- src/main/java/com/ninni/spawn/registry/SpawnVanillaIntegration.java
+++ src/main/java/com/ninni/spawn/registry/SpawnVanillaIntegration.java
@@ -64,7 +64,8 @@ public class SpawnVanillaIntegration {
 
     public static void addResourcePacks() {
         ModContainer modContainer = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(() -> new IllegalStateException("Spawn's ModContainer couldn't be found!"));
-        ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation(MOD_ID, "animal_tweaks"), modContainer, "Animal Tweaks", ResourcePackActivationType.DEFAULT_ENABLED);
+        ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation(MOD_ID, "animation_tweaks"), modContainer, "Animation Tweaks", ResourcePackActivationType.DEFAULT_ENABLED);
+        ResourceManagerHelper.registerBuiltinResourcePack(new ResourceLocation(MOD_ID, "model_tweaks"), modContainer, "Model Tweaks", ResourcePackActivationType.NORMAL);
     }
 
     private static void registerBiomeModifications() {
