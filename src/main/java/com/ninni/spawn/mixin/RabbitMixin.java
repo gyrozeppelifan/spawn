0fc4285fa7c85c836c326f264853048f17980a88
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2024-05-12 14:51:28 +0200

    Mc rabbit fix

---
+++ src/main/java/com/ninni/spawn/mixin/RabbitMixin.java
@@ -0,0 +1,23 @@
+package com.ninni.spawn.mixin;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.VariantHolder;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Rabbit;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Rabbit.class)
+public abstract class RabbitMixin extends Animal implements VariantHolder<Rabbit.Variant> {
+    protected RabbitMixin(EntityType<? extends Animal> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void S$init(EntityType<? extends Rabbit> entityType, Level level, CallbackInfo ci) {
+        this.setMaxUpStep(1);
+    }
+}
